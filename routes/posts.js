038d5dea4078c523b2b1524064d331140dc0c636
Richard <shaodeyu@foxmail.com>
2021-06-26 21:18:46 +0800

    完成了Post 的创建、更新、和删除功能

---
+++ routes/posts.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+
+//create a post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savePost = await newPost.save();
+    res.status(200).json(savePost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//update a post
+/* 思路：在param 中上传post 的id 找到了post ，然后用post 的userId 和 实际的修改人对比 */
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    console.log(post.userId);
+    console.log(req.body.userId);
+
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//like a post
+//get a post
+//get timeline posts
+
+module.exports = router;
