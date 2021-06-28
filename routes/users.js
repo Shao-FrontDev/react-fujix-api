ca0ac00cef32b30cb29f7bac4c1be3cdaf0e8f75
Richard <shaodeyu@foxmail.com>
2021-06-28 21:08:44 +0800

    完成了get a user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("This is User Router");
+//get a user
+
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
+
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //update user
